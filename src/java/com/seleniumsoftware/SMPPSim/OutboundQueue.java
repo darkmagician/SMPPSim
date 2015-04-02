91d8683770aa701ddb9806b9f0d05d94f9c9ff46
scott <flyincloud@gmail.com>
2015-04-02 17:22:05 +0800

    disable the outboundqueue for higher performance

--- src/java/com/seleniumsoftware/SMPPSim/OutboundQueue.java
+++ src/java/com/seleniumsoftware/SMPPSim/OutboundQueue.java
@@ -30,6 +30,11 @@ package com.seleniumsoftware.SMPPSim;
 import com.seleniumsoftware.SMPPSim.exceptions.*;
 import com.seleniumsoftware.SMPPSim.pdu.SubmitSM;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.*;
 import java.util.*;
 
@@ -48,16 +53,72 @@ public class OutboundQueue implements Runnable {
 
 	private LifeCycleManager lcm = smsc.getLcm();
 
-	ArrayList<MessageState> queue;
-
+	private final Map<MessageState,MessageState> queue;
+	private final Lock lock = new ReentrantLock();
+	private final Condition isEmpty = lock.newCondition();
+	private boolean disable = true;
+	
+	
 	public OutboundQueue(int maxsize) {
-		queue = new ArrayList<MessageState>(maxsize);
+		queue = new ConcurrentHashMap<MessageState,MessageState>(maxsize){
+			final AtomicInteger size = new AtomicInteger();
+
+			@Override
+			public int size() {
+				return size.get();
+			}
+
+			@Override
+			public MessageState put(MessageState key, MessageState value) {
+				MessageState ms = super.put(key, value);
+				if(ms == null){
+					size.incrementAndGet();
+				}
+				return ms;
+			}
+
+			@Override
+			public MessageState putIfAbsent(MessageState key, MessageState value) {
+				MessageState ms = super.putIfAbsent(key, value);
+				if(ms == null){
+					size.incrementAndGet();
+				}
+				return ms;
+			}
+
+			@Override
+			public MessageState remove(Object key) {
+				MessageState ms = super.remove(key);
+				if(ms != null){
+					size.decrementAndGet();
+				}
+				return ms;
+			}
+
+			@Override
+			public boolean remove(Object key, Object value) {
+				boolean rc =  super.remove(key, value);
+				if(rc){
+					size.decrementAndGet();
+				}
+				return rc;
+			}
+
+			@Override
+			public void clear() {
+				super.clear();
+				size.set(0);
+			}
+		};
 	}
 
-	public synchronized void addMessageState(MessageState message) throws OutboundQueueFullException {
+	public void addMessageState(MessageState message) throws OutboundQueueFullException {
+		if(disable){
+			return;
+		}
 		logger.finest("OutboundQueue: adding object to queue<" + message.toString() + ">");
 		if (queue.size() < smsc.getOutbound_queue_capacity()) {
-			queue.add(message);
+			queue.put(message, message);
 			logger.fine("Added object to OutboundQueue. Queue now contains " + queue.size() + " object(s)");
 			if (message.isIntermediate_notification_requested()) {
 				SubmitSM p = message.getPdu();
@@ -65,39 +126,54 @@ public class OutboundQueue implements Runnable {
 				logger.info("Intermediate notification requested");
 				smsc.prepareDeliveryReceipt(p, message.getMessage_id(), message.getState(), 1, 1, message.getErr());
 			}
-			notifyAll();
+			try{
+				lock.lock();
+				isEmpty.signalAll();
+			}finally{
+				lock.unlock();
+			}
 		} else
 			throw new OutboundQueueFullException("Request to add to OutboundQueue rejected as to do so would exceed max size of "
 					+ smsc.getOutbound_queue_capacity());
 	}
 
-	public synchronized void setResponseSent(MessageState m) throws MessageStateNotFoundException {
+	public void setResponseSent(MessageState m) throws MessageStateNotFoundException {
+		if(disable){
+			return;
+		}
 		m.setResponseSent(true);
 		updateMessageState(m);
 	}
 
-	public synchronized MessageState getMessageState(MessageState m) throws MessageStateNotFoundException {
+	public MessageState getMessageState(MessageState m) throws MessageStateNotFoundException {
+		if(disable){
+			return m;
+		}
 		//		logger.info("getMessageState:"+m.keyToString());
-		int i = queue.indexOf(m);
+		MessageState message = queue.get(m);
 		//		logger.info("queue pos="+i);
-		if (i > -1) {
-			MessageState message = queue.get(i);
+		if (message != null) {
 			return message;
 		} else {
 			throw new MessageStateNotFoundException();
 		}
 	}
 
-	public synchronized void updateMessageState(MessageState newMs) throws MessageStateNotFoundException {
-		int i = queue.indexOf(newMs);
-		if (i > -1) {
-			queue.set(i, newMs);
+	public void updateMessageState(MessageState newMs) throws MessageStateNotFoundException {
+		if(disable){
+			return;
+		}
+		
+		MessageState message = queue.get(newMs);
+		if (message != null) {
+			queue.put(newMs, newMs);
 		} else {
 			throw new MessageStateNotFoundException();
 		}
 	}
 
 	public MessageState queryMessageState(String message_id, int ton, int npi, String addr) throws MessageStateNotFoundException {
+		
 		MessageState m = new MessageState();
 		m.setMessage_id(message_id);
 		m.setSource_addr_ton(ton);
@@ -106,21 +182,23 @@ public class OutboundQueue implements Runnable {
 		return getMessageState(m);
 	}
 
-	public synchronized void removeMessageState(MessageState m) {
-		int i = queue.indexOf(m);
-		if (i > -1) {
-			MessageState message = queue.remove(i);
-			logger.fine("Removed object from OutboundQueue. Queue now contains " + queue.size() + " object(s)");
+	public void removeMessageState(MessageState m) {
+		if(disable){
+			return;
+		}
+		MessageState message= queue.remove(m);
+		if (message != null) {
+			//logger.fine("Removed object from OutboundQueue. Queue now contains " + queue.size() + " object(s)");
 		} else {
 			logger.warning("Attempt to remove non-existent object from OutboundQueue: " + m.toString());
 		}
 	}
 
-	public synchronized Object[] getAllMessageStates() {
-		return (Object[]) queue.toArray();
+	public Object[] getAllMessageStates() {
+		return (Object[]) queue.keySet().toArray();
 	}
 
-	protected synchronized boolean isEmpty() {
+	protected boolean isEmpty() {
 		return queue.isEmpty();
 	}
 
@@ -143,16 +221,21 @@ public class OutboundQueue implements Runnable {
 	private void processQueue() {
 		Object[] messages = null;
 		int count;
-		MessageState m;
 		long start;
 		long finish;
 		long duration;
 		long sleeptime;
-		synchronized (this) {
-			while (smsc.isRunning() && isEmpty()) {
+			while (smsc.isRunning()) {
 				try {
-					logger.info("Lifecycle Service: OutboundQueue is empty  - waiting");
-					wait();
+					try{
+						lock.lock();
+						if(isEmpty()){
+							logger.info("Lifecycle Service: OutboundQueue is empty  - waiting");
+							isEmpty.await();
+						}
+					}finally{
+						lock.unlock();
+					}
 				} catch (InterruptedException e) {
 					if (smsc.isRunning()) {
 						logger.log(Level.WARNING, "Exception in OutboundQueue: " + e.getMessage(), e);
@@ -160,25 +243,19 @@ public class OutboundQueue implements Runnable {
 				}
 			}
 			start = System.currentTimeMillis();
-			if (smsc.isRunning()) {
-				messages = getAllMessageStates();
-			}
 			// Can allow other threads to compete for lock now since working on array from this point
-			notifyAll();
-		}
 		if (!smsc.isRunning()) {
 			return;
 		}
 		count = messages.length;
 		logger.info("Assessing state of " + count + " messages in the OutboundQueue");
-		for (int i = 0; i < count; i++) {
-			m = (MessageState) messages[i];
+		for (MessageState m: queue.keySet()) {
 			if (!m.responseSent()) {
-				logger.finest("Response not yet sent so ignoring this MessageState object for now");
+				//logger.finest("Response not yet sent so ignoring this MessageState object for now");
 				continue;
 			}
 			if (lcm.messageShouldBeDiscarded(m)) {
-				logger.finest("Disarding OutboundQueue message " + m.toString());
+			//	logger.finest("Disarding OutboundQueue message " + m.toString());
 				removeMessageState(m);
 			} else {
 				byte currentState = m.getState();
